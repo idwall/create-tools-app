53f8d46225fb611abae34dd1ef7c9d81ee917830
Bu Kinoshita <bukinoshita@gmail.com>
2018-06-26 11:55:58 -0300

    add config with dotenv

--- examples/with-apollo/services/init-apollo.js
+++ examples/with-apollo/services/init-apollo.js
@@ -16,7 +16,7 @@ function create(initialState) {
     connectToDevTools: process.browser,
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
     link: new HttpLink({
-      uri: 'https://graphql-pokemon.now.sh/', // Server URL (must be absolute)
+      uri: config.api.url, // Server URL (must be absolute)
       credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers`
     }),
     cache: new InMemoryCache().restore(initialState || {})
